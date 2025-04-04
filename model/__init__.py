c1898cd127031361c4f4a071d92ec23afcbda7e2
GeoRSAI <128791231+GeoRSAI@users.noreply.github.com>
2025-04-04 20:49:57 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,4 @@
 from importlib import import_module
-from .SEANet import seanet
 from .load_r3mamba import unetformer, rs3mamba
 from .load_r3mamba import rsm_ss, CLFDA
 
@@ -7,7 +6,6 @@ from .load_r3mamba import rsm_ss, CLFDA
 
 model_dict = {
     "unetformer":unetformer,
-    "seanet": seanet,
     "rs3mamba": rs3mamba,
     "rsm_ss": rsm_ss,
     "CLFDA":CLFDA,
